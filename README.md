7d0b894965dbf759d86cd768cd2acfc7b6163023
Filip Krunic <fkrunic9@gmail.com>
2023-09-04 20:37:05 -0700

    project setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# Base Apparel Coming Soon Page
+Implementation of [Base Apparel Coming Soon Page](https://www.frontendmentor.io/challenges/base-apparel-coming-soon-page-5d46b47f8db8a7063f9331a0) using Vue and TailwindCSS.
---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="flex items-center justify-center min-h-screen">
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+</style>
